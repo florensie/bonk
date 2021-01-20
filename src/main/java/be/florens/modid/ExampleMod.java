e7279f590d5f929250f83cc1e78057cd15267bf7
Florens Pauwels <github@florens.be>
2021-01-20 16:04:55 +0100

    Initial commit

---
+++ src/main/java/be/florens/modid/ExampleMod.java
@@ -0,0 +1,11 @@
+package be.florens.modid;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello world!");
+	}
+}
